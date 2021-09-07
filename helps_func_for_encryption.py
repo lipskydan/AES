ebec144b81cce198308443291b5245ef6fc8012e
danial <lipsky@knu.ua>
2021-09-07 11:28:53 +0300

    added round_key

--- helps_func_for_encryption.py
+++ helps_func_for_encryption.py
@@ -72,7 +72,41 @@ def right_shift(array, count):
 
 
 def mix_columns(state, inv=False):
-    pass
+    for i in range(nb):
+
+        if not inv:  # encryption
+            s0 = mul_by_02(state[0][i]) ^ mul_by_03(state[1][i]) ^ state[2][i] ^ state[3][i]
+            s1 = state[0][i] ^ mul_by_02(state[1][i]) ^ mul_by_03(state[2][i]) ^ state[3][i]
+            s2 = state[0][i] ^ state[1][i] ^ mul_by_02(state[2][i]) ^ mul_by_03(state[3][i])
+            s3 = mul_by_03(state[0][i]) ^ state[1][i] ^ state[2][i] ^ mul_by_02(state[3][i])
+        else:  # decryption
+            s0 = mul_by_0e(state[0][i]) ^ mul_by_0b(state[1][i]) ^ mul_by_0d(state[2][i]) ^ mul_by_09(state[3][i])
+            s1 = mul_by_09(state[0][i]) ^ mul_by_0e(state[1][i]) ^ mul_by_0b(state[2][i]) ^ mul_by_0d(state[3][i])
+            s2 = mul_by_0d(state[0][i]) ^ mul_by_09(state[1][i]) ^ mul_by_0e(state[2][i]) ^ mul_by_0b(state[3][i])
+            s3 = mul_by_0b(state[0][i]) ^ mul_by_0d(state[1][i]) ^ mul_by_09(state[2][i]) ^ mul_by_0e(state[3][i])
+
+        state[0][i] = s0
+        state[1][i] = s1
+        state[2][i] = s2
+        state[3][i] = s3
+
+    return state
+
+
+def mul_by_02(num):
+    if num < 0x80:
+        res = (num << 1)
+    else:
+        res = (num << 1) ^ 0x1b
+
+    return res % 0x100
+
+
+def mul_by_03(num): return mul_by_02(num) ^ num
+def mul_by_09(num): return mul_by_02(mul_by_02(mul_by_02(num))) ^ num
+def mul_by_0b(num): return mul_by_02(mul_by_02(mul_by_02(num))) ^ mul_by_02(num) ^ num
+def mul_by_0d(num): return mul_by_02(mul_by_02(mul_by_02(num))) ^ mul_by_02(mul_by_02(num)) ^ num
+def mul_by_0e(num): return mul_by_02(mul_by_02(mul_by_02(num))) ^ mul_by_02(mul_by_02(num)) ^ mul_by_02(num)
 
 
 def key_expansion(key):
@@ -80,4 +114,16 @@ def key_expansion(key):
 
 
 def add_round_key(state, key_schedule, round=0):
-    pass
+    for col in range(nk):
+        # nb*round is a shift which indicates start of a part of the KeySchedule
+        s0 = state[0][col] ^ key_schedule[0][nb * round + col]
+        s1 = state[1][col] ^ key_schedule[1][nb * round + col]
+        s2 = state[2][col] ^ key_schedule[2][nb * round + col]
+        s3 = state[3][col] ^ key_schedule[3][nb * round + col]
+
+        state[0][col] = s0
+        state[1][col] = s1
+        state[2][col] = s2
+        state[3][col] = s3
+
+    return state
