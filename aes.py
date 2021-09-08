6566bd0a98d5bde92ca53b026fd39411fc77a9a3
danial <lipsky@knu.ua>
2021-09-08 11:46:36 +0300

    added encrypt method

--- aes.py
+++ aes.py
@@ -1,4 +1,31 @@
-# from const_tables_and_numbers import nb, nk, nr, sbox, inv_sbox
+from const_tables_and_numbers import nb, nk, nr, sbox, inv_sbox
+from helps_func_for_encryption import key_expansion, add_round_key, sub_bytes, shift_rows, mix_columns
+
 
 def encrypt(input_bytes, key):
-   pass
+    state = [[] for j in range(4)]
+    for r in range(4):
+        for c in range(nb):
+            state[r].append(input_bytes[r + 4 * c])
+
+    key_schedule = key_expansion(key)
+
+    state = add_round_key(state, key_schedule)
+
+    rnd = None
+    for rnd in range(1, nr):
+        state = sub_bytes(state)
+        state = shift_rows(state)
+        state = mix_columns(state)
+        state = add_round_key(state, key_schedule, rnd)
+
+    state = sub_bytes(state)
+    state = shift_rows(state)
+    state = add_round_key(state, key_schedule, rnd + 1)
+
+    output = [None for i in range(4 * nb)]
+    for r in range(4):
+        for c in range(nb):
+            output[r + 4 * c] = state[r][c]
+
+    return output
